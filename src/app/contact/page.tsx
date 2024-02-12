c5cfd1ae263273ce854c798945d43717daa47ba0
Jayant Malik <dev.jayantmalik@gmail.com>
2024-02-12 11:57:49 +0530

    removed map

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,7 +7,7 @@ const Contact: NextPage = () => {
       <header>
         <h2 className="h2 article-title">Contact</h2>
       </header>
-      <section className="mapbox" data-mapbox>
+      {/* <section className="mapbox" data-mapbox>
         <figure>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6859.633166560807!2d76.83412474251719!3d30.7235563483544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390f9333773d5863%3A0xf32395e75d7dffc0!2sIT%20Park%20Chandigarh!5e0!3m2!1sen!2sin!4v1694520035735!5m2!1sen!2sin"
@@ -16,7 +16,7 @@ const Contact: NextPage = () => {
             loading="lazy"
           />
         </figure>
-      </section>
+      </section> */}
       <section className="contact-form">
         <h3 className="h3 form-title">Contact Form</h3>
         <ul className="timeline-list">
